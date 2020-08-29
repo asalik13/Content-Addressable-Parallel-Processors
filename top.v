58493c2476b4ba90301b1c34ab3799034f3987ed
Ayush Salik <asalik13@gmail.com>
2020-08-29 20:56:58 +0530

    generate match lines

--- top.v
+++ top.v
@@ -4,6 +4,19 @@ module top (
     output LED,   // User/boot LED next to power LED
     output USBPU  // USB pull-up resistor
 );
+
+  function generate_match_lines;
+    input comparand_bit;
+    input mask_bit;
+    input perform_search;
+    output [1:0] match_line;
+    begin
+    match_line[0] = perform_search && (comparand_bit && mask_bit);
+    match_line[1] = perform_search && ((!comparand_bit) && mask_bit);
+    end
+  endfunction
+
+
     // drive USB pull-up resistor to '0' to disable USB
     assign USBPU = 0;
 
@@ -20,5 +33,18 @@ module top (
     assign mask_wire = mask;
 
     wire perform_search; //controls search
+
+    wire [63:0] match_lines;
+    genvar i;
+    generate
+    for(i=0; i<32;i=i+1) begin
+      assign match_lines[2*i:2*i+2] = generate_match_lines(comparand[i], mask[i], perform_search);
+      end
+    endgenerate
+
+
+
+
+
 
 endmodule

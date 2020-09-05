dd257ba965ae707523073a0a6de256a18b6c5e12
Ayush Salik <asalik13@gmail.com>
2020-09-06 01:44:18 +0530

    indent properly

--- read.v
+++ read.v
@@ -5,22 +5,23 @@ module read (
     output USBPU  // USB pull-up resistor
 );
 
-function read_cell;
-  input reg [31:0] cell_values;
-  input reg tag_bit
-  output wire [31:0] read_lines
+  function read_cell;
+    input reg [31:0] cell_values;
+    input reg tag_bit
+    output wire [31:0] read_lines
 
-  for(i = 0; i<32; i=i+1) begin
-    assign read_lines[i] = tag_bit && cell_values[i];
-  end
-endfunction
+    for(i = 0; i<32; i=i+1) begin
+      assign read_lines[i] = tag_bit && cell_values[i];
+    end
+  endfunction
 
 
-function read;
-  input reg [4095:0] cells;
-  input reg [4095:0] tags;
-  output wire [31:0] read_lines;
-  for(i = 0; i<4096; i=i+1) begin
-    assign read_lines = read_lines | read_cell(cells[i], tags[i])
-  end
-endfunction
+  function read;
+    input reg [4095:0] cells;
+    input reg [4095:0] tags;
+    output wire [31:0] read_lines;
+    for(i = 0; i<4096; i=i+1) begin
+      assign read_lines = read_lines | read_cell(cells[i], tags[i])
+    end
+  endfunction
+endmodule

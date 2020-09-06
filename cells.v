2008b1802232d9895c50563618f0497840573385
Ayush Salik <asalik13@gmail.com>
2020-09-06 08:03:24 +0530

    add write lines to cells

--- cells.v
+++ cells.v
@@ -1,16 +1,39 @@
-
-
-module cells(match_lines, tags);
+module cells(match_lines, write_lines, read_lines, tags);
 input [63:0] match_lines;
-output [99:0] tags;
-reg [31:0] store[0:99];
+input [63:0] write_lines;
+output [4:0] tags;
+output [31:0] read_lines;
+reg [31:0] store[0:4];
+
+
+
+
+
 genvar i,j;
 generate
-for(i=0; i<100; i = i+1) begin
-   for(j=0; j<32; j = j+1) begin
-     assign tags[i] = tags[i] || (match_lines[j+j] & store[i][j]) || (match_lines[j+j+1] & (!store[i][j]));
-   end
+  for(i=0; i<5; i = i+1) begin
+     for(j=0; j<32; j = j+1) begin
+       assign tags[i] = tags[i] || (match_lines[j+j] & store[i][j]) || (match_lines[j+j+1] & (!store[i][j]));
+       assign read_lines[j] = (tags[i] && store[i][j]) || read_lines[j];
+       always @ ( * ) begin
+        if(tags[i])
+          begin
+            if (write_lines[j+j])
+                store[i][j] <= 1;
+            else if (write_lines[j+j+1])
+                store[i][j] <= 0;
+          end
+       end
+
+     end
   end
+
+
+
+
+
+
+
 endgenerate
 
 endmodule

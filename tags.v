8f3b85f42c53a35b97ba6b9fd5534d8f9b609790
Ayush Salik <asalik13@gmail.com>
2020-10-26 03:42:59 +0530

    fixed select_first and some_none

--- tags.v
+++ tags.v
@@ -1,15 +1,15 @@
-module tags(match_lines, set, select_first, tags, CLK);
+module tags(match_lines, set, select_first, tags, some_none, CLK);
 
 input wire [99:0] match_lines;
 input wire select_first;
 input wire set;
 input CLK;
+output [99:0] some_none;
 output reg [99:0] tags;
 
 
 wire [99:0] tag_wires;
 wire [99:0] temp;
-wire [99:0] some_none;
 assign some_none[0] = tag_wires[0];
 srff_behave flip (tag_wires[0], set, match_lines[0], CLK);
 
@@ -18,8 +18,8 @@ genvar i;
 generate
 for(i = 1; i<100; i=i+1) begin: random
   assign some_none[i] = some_none[i-1] || tag_wires[i];
-  assign temp[i] = (some_none[i] & select_first) || match_lines[i];
-  srff_behave flipflop (tag_wires[i], set, match_lines[i], CLK);
+  assign temp[i] = (some_none[i - 1] && select_first) || match_lines[i];
+  srff_behave flipflop (tag_wires[i], set, temp[i], CLK);
 end
 endgenerate
 
--- top_tb.v
+++ top_tb.v
@@ -6,37 +6,44 @@ wire [99:0] tag_wires;
 wire [63:0] mismatch_lines;
 wire [99:0] match_lines;
 wire [31:0] read_lines;
-
-
+wire [99:0] some_none;
+ 
 reg perform_search;
 reg set;
 reg select_first;
 reg [63:0] write_lines;
+
 
 reg [99:0] write_cell;
 reg [99:0] reset_cell;
 
 compare compare_module (CLK, comparand,mask,perform_search, mismatch_lines);
 cells cells_module (match_lines, write_lines, read_lines, mismatch_lines, tag_wires, CLK);
-tags tags_module(match_lines, set, select_first, tag_wires, CLK);
+tags tags_module(match_lines, set, select_first, tag_wires, some_none, CLK);
 
 
 
 
 initial begin
   CLK=0;
-     forever #5 CLK = ~CLK;  
+  forever #5 CLK = ~CLK;  
 end 
 
+initial begin
+  //$dumpfile("top_tb.vcd");
+  //$dumpvars(0,tb);
+  $monitor("%b\n", tag_wires);
+
+end
+
 initial begin 
-    $dumpfile("top_tb.vcd");
-    $dumpvars(0,tb);
-    comparand = 88;
-    mask = 'b1;
+    comparand = 0;
+    mask = 1;
     set = 1;
     #5;
     set = 0;
     perform_search = 1;
+    select_first = 1;
     #100;
     $finish;
 end 

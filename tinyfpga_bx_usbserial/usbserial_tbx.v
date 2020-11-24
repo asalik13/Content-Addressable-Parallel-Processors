6dfe44c8d7094ede1a5cb3c45f71375f27b5137f
Ayush Salik <asalik13@gmail.com>
2020-11-24 22:33:19 +0530

    Added Serial FSM

--- tinyfpga_bx_usbserial/usbserial_tbx.v
+++ tinyfpga_bx_usbserial/usbserial_tbx.v
@@ -1,7 +1,10 @@
 /*
+  USB Serial
+*/
+
+/*
     USB Serial
 
-    Wrapping usb/usb_uart_ice40.v to create a loopback.
 */
 
 module usbserial_tbx (
@@ -24,11 +27,11 @@ module usbserial_tbx (
     pll pll48( .clock_in(pin_clk), .clock_out(clk_48mhz), .locked( clk_locked ) );
 
     // LED
-    reg [22:0] ledCounter;
+    reg [24:0] ledCounter;
     always @(posedge clk_48mhz) begin
         ledCounter <= ledCounter + 1;
     end
-    assign pin_led = ledCounter[ 22 ];
+    assign pin_led = ledCounter[ 24 ];
 
     // Generate reset signal
     reg [5:0] reset_cnt = 0;
@@ -37,18 +40,39 @@ module usbserial_tbx (
         if ( clk_locked )
             reset_cnt <= reset_cnt + reset;
 
-    // uart pipeline in
-    wire [7:0] uart_in_data;
-    wire       uart_in_valid;
-    wire       uart_in_ready;
+  parameter TEXT_LEN=14;
+  // Create the text string
+  reg [7:0] text [0:TEXT_LEN-1];
+  reg [3:0] char_count =4'b0;
 
+    // uart pipeline in
+    reg [7:0] uart_in_data;
+    reg       uart_in_valid = 1'b1;
+    wire       uart_in_ready;
+  initial begin
+    text[0]  <= "H";
+    text[1]  <= "e";
+    text[2]  <= "l";
+    text[3]  <= "l";
+    text[4]  <= "o";
+    text[5]  <= " ";
+    text[6]  <= "W";
+    text[7]  <= "o";
+    text[8]  <= "r";
+    text[9]  <= "l";
+    text[10] <= "d";
+    text[11] <= "!";
+    text[12] <= "\r";
+    text[13] <= "\n";
+  end
+
+
+    wire [7:0] uart_out_data;
+    wire       uart_out_valid;
+    reg       uart_out_ready;
     // assign debug = { uart_in_valid, uart_in_ready, reset, clk_48mhz };
 
-    wire usb_p_tx;
-    wire usb_n_tx;
-    wire usb_p_rx;
-    wire usb_n_rx;
-    wire usb_tx_en;
+
 
     // usb uart - this instanciates the entire USB device.
     usb_uart uart (
@@ -63,15 +87,55 @@ module usbserial_tbx (
         .uart_in_data( uart_in_data ),
         .uart_in_valid( uart_in_valid ),
         .uart_in_ready( uart_in_ready ),
+        .uart_out_data( uart_out_data ),
+        .uart_out_valid( uart_out_valid ),
+        .uart_out_ready( uart_out_ready  )
 
-        .uart_out_data( uart_in_data ),
-        .uart_out_valid( uart_in_valid ),
-        .uart_out_ready( uart_in_ready  )
 
         //.debug( debug )
     );
 
-    // USB Host Detect Pull Up
-    assign pin_pu = 1'b1;
+
+  parameter STATE_WAIT = 1'b0;
+  parameter STATE_TX = 1'b1;
+
+  reg state = STATE_WAIT;
+  reg [23:0] cnt = 24'b0;
+
+always @(posedge clk_48mhz)
+  begin
+      uart_out_ready <= 1;
+      cnt <= cnt +1'b1;
+      case(state)
+      STATE_WAIT:
+        begin
+            uart_in_valid <= 0;
+            if (uart_out_data == "a")
+            begin
+                state <= STATE_TX;
+            end
+        end
+      STATE_TX:
+        begin
+
+          if (uart_in_ready || (~uart_in_valid && ~uart_in_ready))
+          begin
+              uart_in_data <= text[char_count];
+              uart_in_valid <= 1;
+              char_count <= char_count +1;
+              if (char_count +1 == TEXT_LEN)
+              begin 
+                  char_count <= 0;
+                  state <= STATE_WAIT;
+                  cnt <= 24'b0;
+              end 
+          end
+        end
+      endcase
+  end
+ 
+
+  // USB Host Detect Pull Up
+  assign pin_pu = 1'b1;
 
 endmodule
